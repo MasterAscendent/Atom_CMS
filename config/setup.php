826e4358dd2425e988b4977f9055701396ff2585
Steven Hensley II <stevehensley2@gmail.com>
2016-04-28 16:08:45 -0700

    Creating the MySQL Database.

--- config/setup.php
+++ config/setup.php
@@ -2,7 +2,7 @@
 // Setup File:
 
 # Database Connection Here...
-$dbc = mysqli_connect('localhost', 'username', 'password', 'databasename');
+$dbc = mysqli_connect('localhost', 'dev', 'Pinkfloyd7', 'AtomCMS') OR die('Could not connect to MySQL Database: '.mysqli_connect_error());
 
 $site_title = 'Atom CMS 2.0';
 $page_title = 'Home Page';
