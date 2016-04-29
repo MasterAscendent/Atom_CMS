4864c9a348ddd9d08b97725cf792a65243b19813
Steven Hensley II <stevehensley2@gmail.com>
2016-04-29 07:10:01 -0700

    Part 25 - Styling the Login Page with Bootstrap.

--- admin/login.php
+++ admin/login.php
@@ -23,24 +23,34 @@
 
       <h1>Login</h1>
 
-            <form>
-             <div class="form-group">
-               <label for="exampleInputEmail1">Email address</label>
-               <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-             </div>
-             <div class="form-group">
-               <label for="exampleInputPassword1">Password</label>
-               <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-             </div>
+      <div class="row">
 
-             <!--
-              <div class="checkbox">
-               <label>
-                 <input type="checkbox"> Check me out
-               </label>
-             </div> -->
-             <button type="submit" class="btn btn-default">Submit</button>
-            </form>
+        <div class="col-md-4 col-md-offset-4">
+
+          <form>
+           <div class="form-group">
+             <label for="exampleInputEmail1">Email address</label>
+             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+           </div>
+           <div class="form-group">
+             <label for="exampleInputPassword1">Password</label>
+             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+           </div>
+
+           <!--
+            <div class="checkbox">
+             <label>
+               <input type="checkbox"> Check me out
+             </label>
+           </div> -->
+           <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+
+        </div>
+
+      </div>
+
+
 
           </div>
     </div><!-- End Wrap -->
