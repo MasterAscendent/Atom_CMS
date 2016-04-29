75197209506a273dd7dc9b5025ae099f5fb3f5d6
Steven Hensley II <stevehensley2@gmail.com>
2016-04-28 17:13:49 -0700

    Using the URL to Load Content with $_GET.

--- config/setup.php
+++ config/setup.php
@@ -6,8 +6,16 @@ $dbc = mysqli_connect('localhost', 'dev', 'Pinkfloyd7', 'AtomCMS') OR die('Could
 
 $site_title = 'Atom CMS 2.0';
 
+if(isset($_GET['page'])) {
+
+  $pageid = $_GET['page']; // Set $pageid to equel the value given in the URL.
+
+} else {
+  $pageid = 1; // Set $pageid equal to 1 or AKA the Home Page.
+}
+
 # Page Setup
-$q = "SELECT * FROM pages WHERE id = 1";
+$q = "SELECT * FROM pages WHERE id = $pageid";
 $r = mysqli_query($dbc, $q);
 
 $page = mysqli_fetch_assoc($r);
--- index.php
+++ index.php
@@ -18,8 +18,8 @@
         <nav style="border-radius:0px; -webkit-border-radius:0px; -moz-border-radius:0px;" class="navbar navbar-default" role="navigation">
           <div class="container">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
+                <li<?php if($pageid == 1) { echo ' class="active"';} ?>><a href="?page=1">Home</a></li>
+                <li<?php if($pageid == 2) { echo ' class="active"';} ?>><a href="?page=2">About Us</a></li>
                 <li><a href="#">FAQ</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
