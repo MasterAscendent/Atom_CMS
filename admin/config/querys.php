e3ba36adc01f79ed965d120bb070533ea5dd836c
Steven Hensley II <stevehensley2@gmail.com>
2016-04-29 15:11:53 -0700

    Part 56 - INSERT and UPDATE Query for Users in the Admin.

--- admin/config/querys.php
+++ admin/config/querys.php
@@ -1,33 +1,87 @@
 <?php
-  if(isset($_POST['submitted']) == 1) {
 
-    $title = mysqli_real_escape_string($dbc, $_POST['title']);
-    $label = mysqli_real_escape_string($dbc, $_POST['label']);
-    $header = mysqli_real_escape_string($dbc, $_POST['header']);
-    $body = mysqli_real_escape_string($dbc, $_POST['body']);
+  switch ($page) {
+    case 'dashboard':
 
-    if(isset($_POST['id']) != '') {
-      $action = 'updated';
-      $q = "UPDATE pages SET user = $_POST[user], slug = '$_POST[slug]', title = '$title', label = '$label', header = '$header', body = '$body' WHERE id = $_GET[id]";
+    break; // End Dashboard
 
-    } else {
-      $action = 'added';
-      $q = "INSERT INTO pages (user, slug, title, label, header, body) VALUES ($_POST[user], '$_POST[slug]', '$title', '$label', '$header','$body')";
+    case 'pages':
 
+    if(isset($_POST['submitted']) == 1) {
+
+      $title = mysqli_real_escape_string($dbc, $_POST['title']);
+      $label = mysqli_real_escape_string($dbc, $_POST['label']);
+      $header = mysqli_real_escape_string($dbc, $_POST['header']);
+      $body = mysqli_real_escape_string($dbc, $_POST['body']);
+
+      if(isset($_POST['id']) != '') {
+        $action = 'updated';
+        $q = "UPDATE pages SET user = $_POST[user], slug = '$_POST[slug]', title = '$title', label = '$label', header = '$header', body = '$body' WHERE id = $_GET[id]";
+
+      } else {
+        $action = 'added';
+        $q = "INSERT INTO pages (user, slug, title, label, header, body) VALUES ($_POST[user], '$_POST[slug]', '$title', '$label', '$header','$body')";
+
+      }
+
+      $r = mysqli_query($dbc, $q);
+
+      if($r) {
+
+        $message = '<p>Page was' .$action. '!</p>';
+
+      } else {
+
+        $message = '<p>Page could not be ' .$action. ' becuase: '.mysqli_error($dbc);
+        $message .= '<p>'.$q.'</p>';
+
+      }
     }
 
-    $r = mysqli_query($dbc, $q);
+    break; // End Pages
 
-    if($r) {
+    case 'users':
 
-      $message = '<p>Page was' .$action. '!</p>';
+    if(isset($_POST['submitted']) == 1) {
 
-    } else {
+      $first = mysqli_real_escape_string($dbc, $_POST['first']);
+      $last = mysqli_real_escape_string($dbc, $_POST['last']);
 
-      $message = '<p>Page could not be ' .$action. ' becuase: '.mysqli_error($dbc);
-      $message .= '<p>'.$q.'</p>';
+      if(isset($_POST['id']) != '') {
+        $action = 'updated';
+        $q = "UPDATE users SET first = '$first', last = '$last', password = SHA1('$_POST[password]'), status = $_POST[status] WHERE id = $_GET[id]";
 
+      } else {
+        $action = 'added';
+        $q = "INSERT INTO users (first, last, password, status) VALUES ('$first', '$last', SHA1 ('$_POST[password]'),'$_POST[status]')";
+
+      }
+
+      $r = mysqli_query($dbc, $q);
+
+      if($r) {
+
+        $message = '<p>User was' .$action. '!</p>';
+
+      } else {
+
+        $message = '<p>User could not be ' .$action. ' becuase: '.mysqli_error($dbc);
+        $message .= '<p>'.$q.'</p>';
+
+      }
     }
+
+    break; //End Users
+
+    case 'settings':
+
+    break; // End Settings
+
+    default:
+      # code...
+    break;
   }
+
+
 
  ?>
