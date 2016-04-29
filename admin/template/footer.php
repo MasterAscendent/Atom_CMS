5fe4e6b233072338461a7f17bd8ee7766b969723
Steven Hensley II <stevehensley2@gmail.com>
2016-04-29 14:27:40 -0700

    Part 53 - Admin Template System.

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -1,3 +1,9 @@
+</div><!-- End Wrap -->
+
 <footer id="footer">
     <p>This is my footer.</p>
 </footer><!-- End Footer -->
+
+<?php if($debug == 1) { include('widgets/debug.php'); } ?>
+</body>
+</html>
