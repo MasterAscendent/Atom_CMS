3f804f3a4e710b088461221c7539c7dec4f5dc4f
Steven Hensley II <stevehensley2@gmail.com>
2016-04-29 19:47:13 -0700

    Part 76 - Generating a New File name for Uploaded Files.
    Still can't get Dropzones image to work nor the file uploader. I'll work
    on it later.

--- admin/uploads.php
+++ admin/uploads.php
@@ -4,13 +4,18 @@ $ds = DIRECTORY_SEPARATOR; // 1
 
 $storeFolder = '../duploads'; // 2
 
+ext = pathinfo($_FILES['fille']['name'], PATHINFO_EXTENSION);
+$newname = time();
+$random = rand(100,999);
+$name = $newname.$random.'.'.$ext;
+
 if (!empty($_FILES)) {
 
   $tempFile = $_FILES['fille']['tmp_name']; //  3
 
   $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds; // 4
 
-  $targetFile = $targetPath. $_FILES['file']['name']; // 5
+  $targetFile = $targetPath. $name; // 5
 
   move_uploaded_file($tempFile, $targetFile); // 6
 
