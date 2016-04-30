a50235a83546972092c92a314aaf9c8425db0f3d
Steven Hensley II <stevehensley2@gmail.com>
2016-04-29 20:05:36 -0700

    Part 77 - Adding the Filename to the User Table.
    Got Dropzone to work but still no image.

--- admin/uploads.php
+++ admin/uploads.php
@@ -1,17 +1,26 @@
 <?php
 
+include('../config/connection.php');
+
 $ds = DIRECTORY_SEPARATOR; // 1
+$id = $_GET['id'];
 
-$storeFolder = '../duploads'; // 2
+$storeFolder = '../uploads'; // 2
 
-ext = pathinfo($_FILES['fille']['name'], PATHINFO_EXTENSION);
+$ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
 $newname = time();
 $random = rand(100,999);
 $name = $newname.$random.'.'.$ext;
 
+$q = "UPDATE users SET avatar = '$name' WHERE id = $id";
+$r = mysqli_query($dbc, $q);
+
+echo $q.'<br>';
+echo mysqli_error($dbc);
+
 if (!empty($_FILES)) {
 
-  $tempFile = $_FILES['fille']['tmp_name']; //  3
+  $tempFile = $_FILES['file']['tmp_name']; //  3
 
   $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds; // 4
 
